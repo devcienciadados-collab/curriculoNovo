13746d0ef1891d5b4ed405904f7e4824a2295692
Marcos Roberto <marcosdo337@gmail.com>
2025-09-24 13:39:52 -0300

    primeiro envio

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import "../styles/globals.css";
+
+import { setDefaultOptions } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+import { cn } from "@/lib/utils";
+import { ClientProviders } from "@/components/shared/client-providers";
+
+const fontSans = Nunito_Sans({ subsets: ["latin"], variable: "--font-sans" });
+const fontTitle = Nunito({ subsets: ["latin"], variable: "--font-title" });
+
+export const metadata: Metadata = {
+  title: "ResumeCraft",
+  icons: {
+    icon: "/favicon.svg",
+  }
+};
+
+setDefaultOptions({ locale: ptBR });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontTitle.variable,
+          fontSans.variable
+        )}
+      >
+        <ClientProviders>
+          {children}
+        </ClientProviders>
+      </body>
+    </html>
+  );
+}
