8078fe755f889921129ec6dbbde40bd9c7ef8b86
Marcos Roberto <marcosdo337@gmail.com>
2025-11-05 16:25:09 -0300

    teste

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
-  dialect: "postgresql",
   schema: "./src/db/schema.ts",
   out: "./src/drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   }
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      "puppeteer-core",
-      "@sparticuz/chromium",
-    ]
-  },
+  serverExternalPackages: [
+    "puppeteer-core",
+    "@sparticuz/chromium",
+  ],
   images: {
     remotePatterns: [
       {
